665e514c18fe24471a9cfa0185627cb6eddaccbd
apaple <76417012+apaple@users.noreply.github.com>
2022-11-21 20:00:16 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,55 +1,117 @@
-<?php
+<html>
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+<!--  m4teamX-->
 
-define('LARAVEL_START', microtime(true));
+<!-- m4teamX -->
 
-/*
-|--------------------------------------------------------------------------
-| Check If Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is maintenance / demo mode via the "down" command we
-| will require this file so that any prerendered template can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
+ 
 
-if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
-    require __DIR__.'/../storage/framework/maintenance.php';
-}
+<head>
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
+    <title>coba hit web ini</title>
 
-require __DIR__.'/../vendor/autoload.php';
+    <meta charset="UTF-8" />
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-$kernel = $app->make(Kernel::class);
+    <meta property="og:title" content="Hacked by Yukai">
 
-$response = tap($kernel->handle(
-    $request = Request::capture()
-))->send();
+    <meta name="description" content="wh00pz ! your security get down">
 
-$kernel->terminate($request, $response);
+    <meta property="og:description" content="wh00pz ! your security get down">
+
+    <meta property="og:image" content="https://i.imgur.com/6RSyvoJ.jpg%22%3E
+
+    <meta name="theme-color" content="#000">
+
+    <style type='text/css'>
+
+        body,
+
+        a:hover {
+
+            cursor: url(http://cur.cursors-4u.net/symbols/sym-1/sym46.cur), progress !important;
+
+        }
+
+    </style>
+
+</head>
+
+ 
+
+<body bgcolor="black" text="yellow" oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;" onclick="document.getElementById('lagu').play()">
+
+    <style type="text/css">
+
+        center {
+
+            font-family: Courier;
+
+        }
+
+ 
+
+        img {
+
+            opacity: 80%;
+
+        }
+
+ 
+
+        red {
+
+            color: red;
+
+        }
+
+    </style>
+
+    <script language="JavaScript">
+
+        window.onbeforeunload = confirmExit;
+
+ 
+
+        function confirmExit() {
+
+            return "are you sure ? wkwk";
+
+        }
+
+    </script>
+
+    <audio src="https://icsahakarinagar.com/(metrolagu.site)%20-%20Kawaii%20dake%20ja%20Nai%20Shikimori-san%20Opening%20Full%20Honey%20Jet%20Coaster%20by%20Nasuo.mp3" autoplay="true" id="lagu"></audio>
+
+    <table width="100%" height="80%">
+
+        <td>
+
+            <center>
+
+                <small>Burung Hantu Cyber</small><br><br>
+
+                <img src="https://apiyukai.tk/cinta.gif" width="300" height="220"
+
+loading="lazy" onerror="this.style.display = 'none'">
+
+                <br><br>HAPPY BIRTH DAY SANDY<red><i></i></red><br>
+
+                <font size="2">HACKED BY X.1<br></font><br>
+
+                <br><small>
+
+                    <font size="1" color="gray">AKUCINTADIA@GMAIL.COM</font>
+
+                </small><br><br><br>
+
+                <font size="2"><i>Greetz</i><br>
+
+                    ./YUK4I</gont>
+
+        </td>
+
+    </table>
